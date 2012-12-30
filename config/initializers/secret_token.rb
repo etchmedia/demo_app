161e4200cc29d0d91887ce84ea2ae718a528d33c
etchmedia <info@etchmedia.ca>
2012-12-30 15:23:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ed57a85e762194b9159cbc2de47bb248c3ca383d83d0b8632ee2758ee0789a6f7f98f3b2ecd657d463803fe6c80dd508be4f1ba04aed985b06ea90146c254407'
